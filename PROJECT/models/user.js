4cce731923663f486111adbd13e7e12c9a17c3f1
Priya-Harshitha <121441190+Priya-Harshitha@users.noreply.github.com>
2025-06-29 11:51:17 +0530

    Delete PROJECT directory

--- PROJECT/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-  email: String,
-  password: String, // should be hashed
-  role: { type: String, enum: ['renter', 'owner', 'admin'] }
-});
-
-module.exports = mongoose.model('User', UserSchema);
